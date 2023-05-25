6dd952515c6e153647626926fafa3e653070d772
Vladislav Shipovskoi <vladislav.shipovskoy@gmail.com>
2023-05-25 13:36:48 +0700

    change port

--- my-costs-server/src/modules/auth/auth.service.ts
+++ my-costs-server/src/modules/auth/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
     const accessToken = await this.generateAccessToken(user);
     const refreshToken = await this.generateRefreshToken(user._id as string);
     res.cookie('jwt', { ...accessToken, ...refreshToken }, { httpOnly: true });
-    return res.status(HttpStatus.OK).send({ message: 'ok' });
+    return res.status(HttpStatus.OK).send();
   }
 
   async registrationUser(createUserDto: CreateUserDto): Promise<void> {
@@ -82,27 +82,12 @@ export class AuthService {
     }
   }
 
-  parseJwt(token: string) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split('')
-        .map(function (c) {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join(''),
-    );
-
-    return JSON.parse(jsonPayload);
-  }
-
   getTokens(request: Request) {
     return { ...request.cookies.jwt };
   }
 
   async getUserByTokenData(token: string): Promise<User | null> {
-    const parsedToken = this.parseJwt(token);
-    return await this.usersService.findOne(parsedToken.user.username);
+    const tokenPayload = this.jwtService.verify(token);
+    return await this.usersService.findOne(tokenPayload.user.username);
   }
 }
